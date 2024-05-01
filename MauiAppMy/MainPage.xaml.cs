5cfd7088dc659687b01dd85c368878bd6e2b0004
ksenia <kn_ksusha@mail.ru>
2024-05-01 12:15:13 +0300

    qwe

---
+++ MauiAppMy/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using FloidAlgoritm;
+using System.Reflection;
+using static System.Type;
+
+
+namespace MauiAppMy
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        string sborkaAdress;
+        bool assemblyStatus;
+        private Assembly assembly;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void SborkaButtom_Clicked(object sender, EventArgs e)
+        {
+            sborkaAdress=SborkaLabel.Text;
+            
+            InstallAssembly();
+            
+        }
+
+        private void InstallAssembly()
+        {
+
+            
+            UploadNewDll();
+            SborkaInformation.Text = "Получилось!!!";
+            
+        }
+        
+
+        private void UploadNewDll()
+        {
+            Assembly asm = Assembly.LoadFrom(SborkaLabel.Text);
+            assembly = asm;
+            CheckForContract(asm);
+        }
+
+        private void CheckForContract(Assembly asm)
+        {
+            Type[] types = asm.GetTypes();
+
+            bool hasImplementation = types.Any(t => typeof(IFloid).IsAssignableFrom(t) && t.IsClass);
+
+            assemblyStatus = hasImplementation;
+        }
+    }
+}
